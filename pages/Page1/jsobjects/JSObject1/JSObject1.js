b4fb905f3c4327cb2c828da198b4d919243903c6
manish kumar <manish@appsmith.com>
2025-03-06 13:27:44 +0530

    there are some changes over here

--- pages/Page1/jsobjects/JSObject1/JSObject1.js
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -2,11 +2,13 @@ export default {
 	myVar1: [],
 	myVar2: {},
 	myFun1 () {
+		console.log("somechanges");
 		//	write code here
 		//	this.myVar1 = [1,2,3]
 	},
-	
+
 	async myFun2 () {
+		console.log("some other changes");
 		//	use async-await or promises
 		//	await storeValue('varName', 'hello world')
 	}
