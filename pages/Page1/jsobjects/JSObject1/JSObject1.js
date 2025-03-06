5007cbea2dc903ce07d0eb0b0c93f49f7a3a5d79
manish kumar <manish@appsmith.com>
2025-03-06 13:30:25 +0530

    merge one has some changes

--- pages/Page1/jsobjects/JSObject1/JSObject1.js
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -9,6 +9,7 @@ export default {
 
 	async myFun2 () {
 		console.log("some other changes");
+		console.log("I want to create more changes from here on, let's do it?");
 		//	use async-await or promises
 		//	await storeValue('varName', 'hello world')
 	}
