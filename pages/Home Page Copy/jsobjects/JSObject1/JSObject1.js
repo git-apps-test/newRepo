35add7ac5a017bbd1629f39415148320d597364d
manish kumar <manish@appsmith.com>
2025-11-04 05:04:59 +0000

    committing changes

---
+++ pages/Home Page Copy/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,13 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1: () => {
+		return "Hello World!";
+	},
+	myFun2: async () => {
+		const data = await getWorkOrders.data
+		const monthCountArr = new Array(12).fill(0); 
+		data.forEach(({ request_date }) => monthCountArr[new Date(request_date).getMonth()] += 1);
+		return monthCountArr;
+	}
+}
